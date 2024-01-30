e49d9f01fce11b3bb6e360b4f785401a8cb1996f
Amalalex627 <149583586+Amalalex627@users.noreply.github.com>
2024-01-30 11:23:18 +0530

    Add files via upload

---
+++ Arithmeticoperations.py
@@ -0,0 +1,21 @@
+num1 =float(input("Enter the first number: ")) 
+num2 =float(input("Enter the second number: "))
+operation = input("Choose an arithmetic operation (+, -, *, /, ** for exponentiation, // for floor division and % for modulo): ")
+if operation == "+":
+result = num1 + num2
+elif operation == "-":
+result = num1 - num2
+elif operation == "*":
+result = num1 * num2
+elif operation == "/":
+result = num1 / num2
+elif operation == "**":
+result = num1 ** num2
+elif operation == "//":
+result = num1 // num2
+elif operation == "%":
+result = num1 % num2
+else:
+print("Invalid input")
+# Print the result of the arithmetic operation
+print("The result of the arithmetic operation is: ", result)
